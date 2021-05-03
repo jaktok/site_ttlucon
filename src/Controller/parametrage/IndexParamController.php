aa897d26a6c80d610a9fc441694a38eeddfbe0ad
ttlucon <jaktok@gmail.com>
2021-05-03 12:51:10 +0200

    Preparation du parametrage

---
+++ src/Controller/parametrage/IndexParamController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\parametrage;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexParamController extends AbstractController
+{
+    /**
+     * @Route("/index_param", name="index_param")
+     */
+    public function index(): Response
+    {
+        return $this->render('parametrage/index_parametrage/index_parametrage.html.twig', [
+            'controller_name' => 'IndexParamController',
+        ]);
+    }
+}
