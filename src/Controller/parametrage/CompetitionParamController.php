fdee71bc4143d7970911196e9822c5aa47da6d14
ttlucon <jaktok@gmail.com>
2021-05-16 10:00:42 +0200

    creation d une competition

--- src/Controller/parametrage/CompetitionParamController.php
+++ src/Controller/parametrage/CompetitionParamController.php
@@ -5,16 +5,127 @@ namespace App\Controller\parametrage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CompetitionRepository;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Competition;
+use App\Form\CompetitionType;
+use App\Repository\TypeCompetitionRepository;
 
 class CompetitionParamController extends AbstractController
 {
     /**
      * @Route("/dirigeant/param/competition", name="competition_param")
      */
-    public function index(): Response
+    public function index(Request $request,CompetitionRepository $competRepo, TypeCompetitionRepository $typeCompetRepo): Response
     {
-        return $this->render('parametrage/competition_param/competition_param.html.twig', [
-            'controller_name' => 'CompetitionParamController',
+        
+        // recuperation de la liste des competition
+        $listeCompet = $competRepo->findAll();
+        
+        // recuperation de la liste des Types competition
+        $listeTypeCompet = $typeCompetRepo->findAll();
+        
+        foreach ($listeCompet as $compet){
+            $competition = new Competition();
+            $competition = $compet;
+            foreach ($listeTypeCompet as $typeCompet){
+                if ($competition->getTypeCompetition() == $typeCompet){
+                    $competition->setTypeCompetition($typeCompet);
+                }
+            }
+        }
+        
+        $form = $this->createFormBuilder($listeCompet)
+        ->getForm();
+       // dd($listeCompet);
+        return $this->render('parametrage/competition_param/competitions_param.html.twig', [
+            'formCompets' =>  $form->createView(),
+            'compets'   => $listeCompet
         ]);
     }
+    
+    /**
+     * @Route("/dirigeant/param/competition/nouveau/", name="compet_param_nouveau")
+     * @Route("/dirigeant/param/competition/modifier/{id}", name="compet_param_modif")
+     *
+     */
+    public function gerer(Request $request, CompetitionRepository $competRepo, TypeCompetitionRepository $typeCompetRepo, int $id = null): Response
+    {
+        // recuperation de la liste des Types competition
+        $listeTypeCompet = $typeCompetRepo->findAll();
+        
+        if ($id){
+            // recuperation de l enregistrements selectionne
+            $competition = $competRepo->find($id);
+            if ($competition) {
+                foreach ($listeTypeCompet as $typeCompet){
+                    if ($competition->getTypeCompetition() == $typeCompet){
+                        $competition->setTypeCompetition($typeCompet);
+                    }
+                }
+                $form = $this->createForm(CompetitionType::class,$competition);
+                $form->handleRequest($request);
+            }
+        }
+        else{
+            $competition = new Competition();
+            $form = $this->createForm(CompetitionType::class,($competition));
+            $form->handleRequest($request);
+        }
+        
+        if($form->isSubmitted() && $form->isValid()){
+           // dd($competition);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($competition);
+            $entityManager->flush();
+            return $this->redirectToRoute('competition_param');
+        }
+        
+        return $this->render('parametrage/competition_param/fiche_compet_param.html.twig', [
+            'formCompet' => $form->createView(),
+        ]);
+        
+    }
+    
+    /**
+     * @Route("/supprime/compet/{id}", name="supprime_compet")
+     */
+    public function supprimeCompet(Request $request, CompetitionRepository $competRepo, int $id = null): Response
+    {
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        // recuperation de l enregistrements selectionne
+        $compet = $competRepo->find($id);
+        if ($compet) {
+            // On supprimer la compet
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($compet);
+            $entityManager->flush();
+        }
+        
+        return $this->redirectToRoute('competition_param');
+        
+    }
+    
+    /**
+     * @Route("/renseigne/compet/{id}", name="renseigne_compet")
+     */
+    public function renseignerCompet(Request $request, CompetitionRepository $competRepo, int $id = null): Response
+    {
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        // recuperation de l enregistrements selectionne
+        $compet = $competRepo->find($id);
+        if ($compet) {
+        
+        }
+        
+        return $this->redirectToRoute('competition_param');
+        
+    }
+    
+    
+    
 }
---
+++ src/Form/CompetitionType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Competition;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class CompetitionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('num_journee')
+            ->add('date',DateTimeType::class,[
+                'widget' => 'single_text',
+                'attr' => ['class' => 'js-datepicker'],
+                'label' => 'Date competition',
+                'format' => 'dd/MM/yyyy',
+                'html5'  => false
+            ])
+            ->add('type_competition',EntityType::class, array(
+                'class' => 'App\Entity\TypeCompetition',
+                'label' => 'Competition',
+                'choice_label' => function ($user) {
+                return $user->getLibelle();
+                },
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Competition::class,
+        ]);
+    }
+}
