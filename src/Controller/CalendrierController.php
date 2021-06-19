a8e49216193a1ecc5dd7b3448dfb58d03c3f7769
ttlucon <jaktok@gmail.com>
2021-06-19 21:38:20 +0200

    rajout onglet calendrier

--- src/Controller/CalendrierController.php
+++ src/Controller/CalendrierController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Repository\EquipeTypeRepository;
 use App\Repository\RencontresRepository;
 use App\Repository\CompetitionRepository;
+use App\Entity\Competition;
+use App\Repository\CategoriesRepository;
 
 
 class CalendrierController extends AbstractController
@@ -18,7 +20,7 @@ class CalendrierController extends AbstractController
     /**
      * @Route("/calendrier/{cat}", name="calendrier")
      */
-    public function index(Request $request,CompetitionRepository $competRepo, RencontresRepository $rencontreRepo,EquipeTypeRepository $equipeRepo,$cat=null): Response
+    public function index(Request $request,CategoriesRepository $categoriesRepo, CompetitionRepository $competRepo, RencontresRepository $rencontreRepo,EquipeTypeRepository $equipeRepo,$cat=null): Response
     {
         switch ($cat) {
             case "Adulte":
@@ -34,6 +36,22 @@ class CalendrierController extends AbstractController
                 $this->categorie = "Adulte";
         }
 
+        // recuperation de la liste des competition
+        $listeCompet = $competRepo->findAll();
+        
+        // recuperation de la liste des Types competition
+        $listeCategories = $categoriesRepo->findAll();
+        
+        foreach ($listeCompet as $compet){
+            $competition = new Competition();
+            $competition = $compet;
+            foreach ($listeCategories as $categorie){
+                if ($competition->getCategories() == $categorie){
+                    $competition->setCategories($categorie);
+                }
+            }
+        }
+        
         
         if( $this->categorie != "Adapte"){
         
@@ -41,13 +59,13 @@ class CalendrierController extends AbstractController
             
             // test pour dev phase 2 sur la phase 1
             $calendrierPhase2 = $rencontreRepo->findByPhase(2);
-            //$calendrierPhase2 = $rencontreRepo->findByPhase(1);
             return $this->render('calendrier/calendrier.html.twig', [
                 'rencontres' => $rencontreRepo->findAll(),
                 'calendrierPhase1' => $calendrierPhase1,
                 'calendrierPhase2' => $calendrierPhase2,
                 'equipes' => $equipeRepo->findAll(),
                 'categorie' => $this->categorie,
+                'compets'   => $listeCompet,
             ]);
         }
         else{
@@ -56,6 +74,7 @@ class CalendrierController extends AbstractController
         return $this->render('calendrier/calendrier.html.twig', [
             'compets'   => $listeCompet,
             'categorie' => $this->categorie,
+            'compets'   => $listeCompet,
         ]);
         }
 
