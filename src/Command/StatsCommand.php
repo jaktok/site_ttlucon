2b039bc4b7de974d282b2c83d927a73a853a9969
jaktok <47668212+jaktok@users.noreply.github.com>
2021-11-08 13:51:52 +0100

    Add files via upload

--- src/Command/StatsCommand.php
+++ src/Command/StatsCommand.php
@@ -80,7 +80,7 @@ class StatsCommand extends Command
                     }
                 }
                 
-                $nbMatchsEnregistres = $matchsRepo->findVictoiresByIdJoueur($joueurTTL->getId()) + $matchsRepo->findDefaitesByIdJoueur($joueurTTL->getId());
+                $nbMatchsEnregistres = $this->matchsRepo->findVictoiresByIdJoueur($joueurTTL->getId()) + $this->matchsRepo->findDefaitesByIdJoueur($joueurTTL->getId());
                 // rajout du local dans le cas ou les derniers matchs n'ont pas été remontés par FFTT ....
                 if( (!empty($partieJoueurByLicence) && (($partieJoueurByLicence["vict"]+$partieJoueurByLicence["def"]<=0) || $partieJoueurByLicence["vict"]+$partieJoueurByLicence["def"] < $nbMatchsEnregistres) ) || empty($partieJoueurByLicence)){
                     $tabResultLocal["vict"] = $this->matchsRepo->findVictoiresByIdJoueur($joueurTTL->getId());
